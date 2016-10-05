d185b33414468b1f252960174d474e19dc722b46
Mladen Vojnovic <mvojnovic@student.tgm.ac.at>
2016-10-05 12:21:28 +0200

    shiet

--- J&R_M/Assets/NewBehaviourScript.cs
+++ J&R_M/Assets/NewBehaviourScript.cs
@@ -16,38 +16,36 @@ public class NewBehaviourScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        
+
 
         //MOVEMENT LEFT RIGHT
         if (Input.GetKey(KeyCode.A))
         {
-            movex = -1;
+            movex = movex-1;
             print("links");
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            movex = 1;
+            movex = movex+1;
             print("rechts");
         }
         else
         {
-            movex = 0;
+            if (movex < 0)
+            {
+                movex = movex + 1;
+            }else
+            {
+                movex = movex - 1;
+            }
         }
 
         //MOVEMENT JUMP
         if (Input.GetKey(KeyCode.Space) && rgbdy.velocity.y == 0)
         {
-            movey = 6 ;
-            rgbdy.velocity = new Vector2(Speed * movex, movey);
-        }else if(rgbdy.velocity.y > 0)
-        {
-            movey = movey-0.25f;
-            rgbdy.velocity = new Vector2(Speed * movex, movey);
+            rgbdy.AddForce(new Vector2(0, 0.00055f), ForceMode2D.Impulse);
         }
-        if (rgbdy.GetComponent<Rigidbody2D>().velocity.y == 0)
-        {
-
-        }
-
         movex = Input.GetAxis("Horizontal");
         rgbdy.velocity = new Vector2(Speed * movex, rgbdy.velocity.y);
 
@@ -56,6 +54,9 @@ public class NewBehaviourScript : MonoBehaviour {
         {
            rgbdy.velocity = new Vector2(Speed * movex, rgbdy.velocity.y * 1.05f);
         }
+        print(rgbdy.velocity.y + "  " + rgbdy.velocity.x);
+
+
 
     }
     public Rigidbody2D getRgbdy()
