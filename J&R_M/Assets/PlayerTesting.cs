39e2d112dcbc9a64c47a36b1f38f662cc35f8746
Mladen Vojnovic <mvojnovic@student.tgm.ac.at>
2016-10-05 14:11:04 +0200

    Resources added

--- J&R_M/Assets/PlayerTesting.cs
+++ J&R_M/Assets/PlayerTesting.cs
@@ -1,24 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerTesting : MonoBehaviour {
 
     public float Speed = 1f;
     private float movex = 0f;
     private float movey = 0f;
     private Rigidbody2D rgbdy;
+    private SpriteRenderer sr;
 
     // Use this for initialization
     void Start() {
         rgbdy = GetComponent<Rigidbody2D>();
         rgbdy.fixedAngle = true;
+        //sr = GetComponent<SpriteRenderer>();
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("dirt", typeof(Sprite)) as Sprite;
+        //gameObject.GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load("random_thing", typeof(Sprite));
+    }
+
+    void setSprite(string text)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("random_thing.psd", typeof(Sprite)) as Sprite;
     }
 
     // Update is called once per frame
     void Update() {
-        
 
 
+        //setSprite("");
         //MOVEMENT LEFT RIGHT
         if (Input.GetKey(KeyCode.A))
         {
