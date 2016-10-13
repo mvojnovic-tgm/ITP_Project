187a457759a59ad6a7a7b4831cc36587a553f6c3
Mladen Vojnovic <mvojnovic@student.tgm.ac.at>
2016-10-13 10:10:00 +0200

    es geht auch ohne bitch

---
+++ J&R_M/Assets/move_it.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move_it : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector2(transform.position.x+0.92f, 0f);
+            
+        }else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector2(transform.position.x-0.92f, 0f);
+        }
+	}
+}
